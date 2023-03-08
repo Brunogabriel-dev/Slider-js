8e7af3d5dc25b0763bbc656de0b09aae44c7ee7b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-03-07 22:30:31 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ document.querySelector('.slider--width').style.width =
   `calc(100vw * ${totalSlides})`;
 document.querySelector('.slider--controls').style.height = 
   `${document.querySelector('.slider').clientHeight}px`;
+
+  function goPrev() {
+    currentSlide--;
+    if(currentSlide < 0) {
+      currentSlide = totalSlides - 1;
+    }
+    updateMargin();
+  }
+  function goNext() {
+    currentSlide++;
+    if(currentSlide > (totalSlides-1)) {
+      currentSlide = 0;
+    }
+    updateMargin();
+  }
+
+  function updateMargin() {
+
+  }
